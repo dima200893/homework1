9a6174cc5e3fe35cb25340d65e7b81292a9ddb4a
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 17:09:25 +0300

    zdacha c list

---
+++ less8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("vvedite chislo N= ");
+int N = Convert.ToInt32(Console.ReadLine());
+string list=" ";
+int count=2;
+int test= count % 2;
+while(count<=N)
+{
+    if (test==0)
+    {
+        Console.Write(list + " " + count++);
+    }
+    count++;
+}
