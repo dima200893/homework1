a7e0383b67e1e4271b46f0604dcd3b86d679ab6f
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 16:17:05 +0300

    Zada4a 4et ne4et

---
+++ less6/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine("VVedite chislo =  ");
+ int number=Convert.ToInt32(Console.ReadLine()); 
+ int test=number%2;
+ if (test==0)
+ {
+    Console.WriteLine("Dannoe chislo 4etnoe");
+ }
+ else
+ {
+    Console.WriteLine("Dannoe chislo Ne4etnoe");   
+ }
