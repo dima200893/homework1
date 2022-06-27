d2441da9cf0626f9519a33956102e01a219b3e82
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 15:57:19 +0300

    Zada4a na max

---
+++ less2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("number 1= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number 2= ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("Number 1 -> max");
+}
+else
+{
+    Console.WriteLine("Number 2 -> max");
+}
