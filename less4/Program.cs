86db09e0e96f791ae351ea5642b0e809a4dbd1a2
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 16:09:14 +0300

    Zada4a 3 chicla ->max

---
+++ less4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("number 1= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number 2= ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("number 3= ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a>b && a>c)
+{
+    Console.WriteLine("Number 1 -> max");
+}
+else
+{
+    if(b>c)
+    {
+        Console.WriteLine("Number 2 -> max");
+    }
+    else
+    {
+        Console.WriteLine("Number 3 -> max");
+    }
+}
